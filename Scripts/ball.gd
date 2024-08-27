d6f095ff814c537cb4a8b7b49fdf2dd502bc9d89
Oreo-Cat <BenjaminHutchinson123@outlook.com>
2024-08-27 13:06:03 +0100

    Added new player

--- Scripts/ball.gd
+++ Scripts/ball.gd
@@ -1,31 +1,29 @@
-extends CharacterBody3D
+extends RigidBody3D
 
 
 #Instance and import variables
 const SPEED = 5.0
-const FRICTION = 1
 const JUMP_VELOCITY = 10
+const DEFAULT_ZOOM = 5
 
-@onready var ball = $Ball
-@onready var camera_pivot = $CameraPivot
-@onready var spring_arm = $CameraPivot/SpringArm3D
+@onready var arrow = $"../ArrowPivot/Arrow"
+@onready var arrow_pivot = $"../ArrowPivot"
+@onready var camera_pivot_v = $"../CameraPivotV"
+@onready var camera_pivot_h = $"../CameraPivotV/CameraPivotH"
+@onready var spring_arm = $"../CameraPivotV/CameraPivotH/SpringArm3D"
 @export var sensitivity = -0.3
 @export var camera_is_locked = false
 @export var zoom_speed = 0.3
-@export var default_zoom = 5
-
-var gravity = ProjectSettings.get_setting("physics/3d/default_gravity")
-
 
 #Camera controls
 func _input(event):
 	if Input.is_mouse_button_pressed(2) or camera_is_locked:
 		Input.mouse_mode = Input.MOUSE_MODE_CAPTURED
 		if event is InputEventMouseMotion:
-			rotate_y(deg_to_rad(event.relative.x * sensitivity))
-			camera_pivot.rotate_x(deg_to_rad(event.relative.y * sensitivity))
-			camera_pivot.rotation.x = clamp(camera_pivot.rotation.x, deg_to_rad(-90), deg_to_rad(45))
-			ball.rotate_y(deg_to_rad(event.relative.x * sensitivity * -1))
+			camera_pivot_v.rotate_y(deg_to_rad(event.relative.x * sensitivity))
+			camera_pivot_h.rotate_x(deg_to_rad(event.relative.y * sensitivity))
+			camera_pivot_h.rotation.x = clamp(camera_pivot_h.rotation.x, deg_to_rad(-90), deg_to_rad(45))
+
 	else:
 		Input.mouse_mode = Input.MOUSE_MODE_VISIBLE
 		
@@ -49,33 +47,31 @@ func _input(event):
 			
 			# Snap to default zoom
 			if event.button_index == MOUSE_BUTTON_MIDDLE:
-				spring_arm.spring_length = default_zoom
+				spring_arm.spring_length = DEFAULT_ZOOM
 			
 	
 			
 func _physics_process(delta):
+	camera_pivot_v.position = position
+	arrow_pivot.position = position
 	
-	if not is_on_floor():
-		velocity.y -= gravity * delta
-		
 	#Will be replaced with correct controls
-	if Input.is_action_just_pressed("DevJump") and is_on_floor():
-		velocity.y = JUMP_VELOCITY
-		
+	if Input.is_action_just_pressed("DevJump"):
+		linear_velocity.y = arrow.position.z*-1
+		linear_velocity.z = arrow.global_position.z - global_position.z
+		linear_velocity.x = arrow.global_position.x - global_position.x
+		angular_velocity = Vector3(randf()*arrow.position.z, randf()*arrow.position.z, randf()*arrow.position.z)
+
 	if Input.is_action_just_pressed("DevQuit"):
 		get_tree().quit()
 
 	var input_dir = Input.get_vector("DevLeft", "DevRight", "DevUp", "DevDown")
-	var direction = (transform.basis * Vector3(input_dir.x, 0, input_dir.y)).normalized()
-	if direction:
-		velocity.x = direction.x * SPEED
-		velocity.z = direction.z * SPEED
-	else:
-		velocity.x = move_toward(velocity.x, 0, SPEED/FRICTION)
-		velocity.z = move_toward(velocity.z, 0, SPEED/FRICTION)
-	
-	#Rotates the ball - need to fix rotating when collided
-	ball.rotate_z(deg_to_rad(input_dir.x*SPEED*-1))
-	ball.rotate_x(deg_to_rad(input_dir.y*SPEED))
 
-	move_and_slide()
+	#Rotates and moves the arrow
+	arrow_pivot.rotate_y(deg_to_rad(input_dir.x)*3/arrow.position.z)
+	arrow.position.z += input_dir.y * 0.1
+	arrow.position.z = clamp(arrow.position.z, -5, -1)
+	#Scale arrow at far distances to be easily visible
+	arrow.scale.z = (arrow.position.z-3)/-60
+	arrow.scale.x = (arrow.position.z-3)/-60
+	arrow.scale.y = (arrow.position.z-3)/-60
