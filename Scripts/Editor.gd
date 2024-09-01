de7095d0d5faa7d78014f60f50e1f741e9836da3
ValleyDragon888 <ValleyDragon888@gmail.com>
2024-09-01 16:20:40 +0100

    Loading tracks now

--- Scripts/Editor.gd
+++ Scripts/Editor.gd
@@ -8,6 +8,8 @@ extends Node3D
 @onready var block_instances: Array[EditorBlockInstance] = []
 @onready var save_path = ""
 @onready var save_as_dialog = $CanvasLayer/SaveAsDialog
+@onready var load_confirmation_dialog = $CanvasLayer/LoadDialog
+@onready var load_fileselector = $CanvasLayer/LoadFileSelectorDialog
 @export var valid_blocks = [
 		"TrackStraight",
 		"TrackCornerSharp"
@@ -50,6 +52,13 @@ func save():
 	file.store_string(json_dict_str)
 	file.close()
 
+func block_instance_from_json(a: Dictionary, id: int) -> EditorBlockInstance:
+	return EditorBlockInstance.new(
+		id,
+		Vector3(a["position"][0], a["position"][1], a["position"][2]),
+		Vector3(a["rotation"][0], a["rotation"][1], a["rotation"][2]),
+		a["type"]
+	)
 
 func _on_show_hide_button_pressed():
 	# lazy. this checks if text is "show blocks"
@@ -75,3 +84,26 @@ func _on_save_path_button_pressed():
 
 func _on_save_as_dialog_file_selected(path):
 	save_path = path
+
+func _on_load_button_pressed():
+	load_confirmation_dialog.popup()
+
+func _on_load_dialog_confirmed():
+	load_fileselector.popup()
+
+func _on_load_file_selector_dialog_file_selected(path):
+	# Read in and decode json
+	var file = FileAccess.open(path, FileAccess.READ)
+	var contents = file.get_as_text()
+	var json_decoded = JSON.parse_string(contents)
+	
+	# Remove blocks in scene tree and script cache
+	block_instances = []
+	for child in $AddedBlocksRoot.get_children():
+		$AddedBlocksRoot.remove_child(child)
+	
+	# Add blocks from file.
+	for i in len(json_decoded["blocks"]):
+		# The last arg of blkinstance from json is the id.
+		block_instances.append(block_instance_from_json(json_decoded["blocks"][i], i))
+		$AddedBlocksRoot.add_child(block_instances[-1].node())
