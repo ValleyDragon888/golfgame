20c87f7e5692c584f1f7cf2a5dfe362bae418b32
Oreo-Cat <BenjaminHutchinson123@outlook.com>
2024-09-04 17:50:34 +0100

    Ditto (forgot to stage changes)

--- Scripts/ball.gd
+++ Scripts/ball.gd
@@ -12,6 +12,7 @@ const DEFAULT_ZOOM = 5
 @export var zoom_speed = 0.3
 var arrow_material = preload("res://Assets/ArrowMaterial.tres")
 var has_velocity = false
+var just_released = false
 
 #Camera controls
 func _input(event):
@@ -58,14 +59,28 @@ func _physics_process(delta):
 
 #May be replaced with correct controls
 	if Input.is_action_just_pressed("Up") and not has_velocity:
+		just_released = false
 		has_velocity = true
 		linear_velocity.y = arrow.position.z*-1
 		linear_velocity.z = (arrow.global_position.z - global_position.z) * 10
 		linear_velocity.x = (arrow.global_position.x - global_position.x) * 10
 		angular_velocity = Vector3(randf()*arrow.position.z, randf()*arrow.position.z, randf()*arrow.position.z)
+	if Input.is_action_just_released("Up"):
+		just_released = true
+
 
 	if abs(linear_velocity.x) < 0.1 and abs(linear_velocity.y) < 0.1 and abs(linear_velocity.z) < 0.1:
 		has_velocity = false
+
+	if Input.is_action_pressed("Up") and has_velocity and just_released:
+		Engine.time_scale = 3
+	else:
+		Engine.time_scale = 1
+
+	if has_velocity:
+		arrow.visible = false
+	else:
+		arrow.visible = true
 
 #Quit the game
 	if Input.is_action_just_pressed("Quit"):
