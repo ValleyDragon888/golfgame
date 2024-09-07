873e8960aff0ccd8d6187023bf9f8b5319fc9036
Oreo-Cat <BenjaminHutchinson123@outlook.com>
2024-09-07 15:47:15 +0100

    changed selected block to start block

--- Scripts/GlobalVariables.gd
+++ Scripts/GlobalVariables.gd
@@ -2,5 +2,5 @@ extends Node
 
 #Global variables that are accessible anywhere - use GlobalVariables.variable
 
-var block_selected: String = "End"
+var block_selected: String = "Start"
 var mouse_hovered: bool = false
