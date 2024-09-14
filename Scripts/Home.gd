63c47761b59b4a98e359f247a6fb2f67a916355e
ValleyDragon888 <ValleyDragon888@gmail.com>
2024-09-14 06:45:56 +0100

    Add accessing editor from home screen

--- Scripts/Home.gd
+++ Scripts/Home.gd
@@ -2,6 +2,7 @@ extends Node3D
 
 @onready var mode = "HomeScreen"
 @onready var mouse_position_y
+@onready var editor = preload("res://Scenes/Editor.tscn")
 
 # Called when the node enters the scene tree for the first time.
 func _ready():
@@ -9,9 +10,10 @@ func _ready():
 
 # Called every frame. 'delta' is the elapsed time since the previous frame.
 func _process(delta):
-	$Background/CameraOrigin.rotation_degrees.y += 10 * delta
-	$Background/CameraOrigin/Camera3D.look_at($Background/CameraOrigin.position)
-	$Background/CameraOrigin.rotation_degrees.x = lerp($Background/CameraOrigin.rotation_degrees.x, mouse_position_y, 0.01)
+	pass
+	#$Background/CameraOrigin.rotation_degrees.y += 10 * delta
+	#$Background/CameraOrigin/Camera3D.look_at($Background/CameraOrigin.position)
+	#$Background/CameraOrigin.rotation_degrees.x = lerp($Background/CameraOrigin.rotation_degrees.x, mouse_position_y, 0.01)
 
 func _input(event):
 	if event is InputEventMouseMotion:
@@ -26,3 +28,8 @@ func _on_back_pressed():
 	mode = "HomeScreen"
 	$Credits.hide()
 	$HomeScreen.show()
+
+
+func _on_editor_pressed():
+	get_tree().change_scene_to_packed(editor)
+	
