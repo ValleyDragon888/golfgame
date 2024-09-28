a9367a0349cd84fedf938f49cb01b47ea0562923
ValleyDragon888 <ValleyDragon888@gmail.com>
2024-09-28 12:44:56 +0100

    Rename campaign tracks

--- Scripts/GlobalVariables.gd
+++ Scripts/GlobalVariables.gd
@@ -21,7 +21,7 @@ var trackplayer = preload("res://Scenes/TrackPlayer.tscn")
 const scene_blocks = ["MovingPiece"]
 const campaigns = {
 	"Main Campaign": [
-		{"1": "res://Tracks/JSON/MainCampaign/01.json"},
-		{"2": "res://Tracks/JSON/MainCampaign/02.json"}
+		{"1 - Introduction": "res://Tracks/JSON/MainCampaign/01.json"},
+		{"2 - Ramps & Turns": "res://Tracks/JSON/MainCampaign/02.json"}
 	]
 }
