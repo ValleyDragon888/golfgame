6a66cf9063af5b08322d681ce65c68d8d46e1d42
ValleyDragon888 <ValleyDragon888@gmail.com>
2024-10-08 19:34:30 +0100

    Add more multiplayer logic

--- Scenes/MultiplayerTrackPlayer.gd
+++ Scenes/MultiplayerTrackPlayer.gd
@@ -1,15 +1,21 @@
 extends Node3D
 
 const num_players = 2
+var turn = 0
 
 # Called when the node enters the scene tree for the first time.
 func _ready():
+	# debug, remove later
+	GlobalVariables.current_track = "res://Tracks/JSON/MainCampaign/01.json"
+	
 	for i in range(num_players):
 		var new_player = SubViewport.new()
 		new_player.add_child(GlobalVariables.trackplayer.instantiate())
 		new_player.name = "Player" + str(i)
 		$Players.add_child(new_player)
-
+		
+	$ViewportDisplayer.texture = ViewportTexture.new()
+	$ViewportDisplayer.texture = $Players/Player0.get_texture()
 
 # Called every frame. 'delta' is the elapsed time since the previous frame.
 func _process(delta):
