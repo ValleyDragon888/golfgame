8f2d8158e3ed934ab3992008ab5c04b31d2521af
ValleyDragon888 <ValleyDragon888@gmail.com>
2024-10-10 06:40:23 +0100

    switch from viewports to plain camera switching

--- Scenes/MultiplayerTrackPlayer.gd
+++ Scenes/MultiplayerTrackPlayer.gd
@@ -10,15 +10,26 @@ func _ready():
 	GlobalVariables.trackplayer_requested_scene_load = "res://Tracks/JSON/MainCampaign/01.json"
 	
 	for i in range(num_players):
-		var new_player = SubViewport.new()
-		new_player.size = Vector2(1024, 600)
+		var new_player = Node3D.new()
+		new_player.position.x = i*1000
 		new_player.add_child(GlobalVariables.trackplayer.instantiate())
 		new_player.name = "Player" + str(i)
 		$Players.add_child(new_player)
-		
-	#$ViewportDisplayer.texture = ViewportTexture.new()
-	#$ViewportDisplayer.texture = $Players/Player0.get_texture()
+	
+	update_turn()
+
+func update_turn():
+	for i in range(num_players):
+		if i == turn:
+			var camera = get_node("/root/MultiplayerTrackPlayer/Players/Player" + str(i) + "/TrackPlayer/Player/CameraPivotV/CameraPivotH/SpringArm3D/Camera3D")
+			camera.current = true
+		else:
+			var camera = get_node("/root/MultiplayerTrackPlayer/Players/Player" + str(i) + "/TrackPlayer/Player/CameraPivotV/CameraPivotH/SpringArm3D/Camera3D")
+			camera.current = false
 
 # Called every frame. 'delta' is the elapsed time since the previous frame.
 func _process(delta):
-	pass
+	if Input.is_key_pressed(KEY_Q):
+		print("chonk")
+		turn = 1
+		update_turn()
