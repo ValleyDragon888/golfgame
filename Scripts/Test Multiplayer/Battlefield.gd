7ddc96956ac4179bade90a2168ce8993df86bdf8
Oreo-Cat <BenjaminHutchinson123@outlook.com>
2024-10-24 14:03:36 +0100

    Fixed (ish) Multiplayer spawner

---
+++ Scripts/Test Multiplayer/Battlefield.gd
@@ -0,0 +1,21 @@
+extends Node2D
+
+var peer = ENetMultiplayerPeer.new()
+@export var player_scene: PackedScene
+
+
+func _on_host_pressed():
+	peer.create_server(135)
+	multiplayer.multiplayer_peer = peer
+	multiplayer.peer_connected.connect(_add_player)
+	_add_player()
+
+func _add_player(id = 1):
+	var player = player_scene.instantiate()
+	player.name = str(id)
+	call_deferred("add_child",player)
+	print("id: ",id,"   name: ",player.name)
+
+func _on_join_pressed():
+	peer.create_client("localhost",135)
+	multiplayer.multiplayer_peer = peer
---
+++ Scripts/Test Multiplayer/Player.gd
@@ -0,0 +1,46 @@
+extends CharacterBody2D
+
+var offset = Vector2.ZERO
+@export var collected: Array = []
+@export var activated = false
+
+func _enter_tree():
+	print(name)
+	set_multiplayer_authority(name.to_int())
+	if is_multiplayer_authority():
+		$AuthDebug.show()
+	else:
+		$AuthDebug.hide()
+
+func _physics_process(delta):
+#Movement controls
+	if is_multiplayer_authority():
+		global_position = offset
+		offset += Input.get_vector("ui_left","ui_right","ui_up","ui_down").normalized() * 300 * delta
+	$VarDebug.text = str("Active:", Global.active, "         Collected:", collected)
+	move_and_slide()
+
+#Pickup item controls
+	for item in len(collected):
+		collected[item].global_position = lerp(collected[item].global_position,global_position,0.05)
+		collected[item].gravity_scale = 0
+		if Input.is_mouse_button_pressed(MOUSE_BUTTON_LEFT):
+			activated = true
+		if activated and not Global.active.has(collected[item]):
+			Global.active.append(collected[item])
+
+func _on_area_2d_body_entered(body):
+#Checks to see what happens when you collide with an object
+	if body.name.match("**Bomb****"):
+		if Global.active.has(body) and collected.has(body):
+			print("Activated")
+		elif Global.active.has(body) and not collected.has(body):
+			print("Exploded")
+			$PlayerSprite.hide()
+		elif not Global.active.has(body) and collected.has(body):
+			print("Collected")
+		elif not Global.active.has(body) and not collected.has(body):
+			print("Not yet collected")
+			collected.append(body)
+
+
